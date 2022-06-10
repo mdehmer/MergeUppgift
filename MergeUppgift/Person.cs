37231211bb2b6c0de37354891c1b1013af174b8b
ricky <ricky@DESKTOP-UFJ4HP9>
2022-06-10 14:22:12 +0200

    ny instance + ny constructor.

--- MergeUppgift/Person.cs
+++ MergeUppgift/Person.cs
@@ -8,6 +8,12 @@ namespace MergeUppgift
 {
     internal class Person
     {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
         public string Name { get; set; }
         public int Age { get; set; }
     }
